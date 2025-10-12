1eb2072b504cbf27bba27b438ed26dbb73e95e6a
ARJ2211 <aayushrj22@gmail.com>
2025-10-12 18:17:36 -0400

    changes for settings, making it private

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,6 @@
 //YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!! DO NOT FORGET TO CHANGE THE DATABASE NAME!!!!!
 export const mongoConfig = {
-    serverUrl: "mongodb://localhost:27017/",
+    serverUrl:
+        "mongodb+srv://aayushrj22_db_user:cR9TDX4ngPK4UhR0@slackoverflow.ypbb6db.mongodb.net/?retryWrites=true&w=majority&appName=SlackOverflow",
     database: "SlackOverflow",
 };
