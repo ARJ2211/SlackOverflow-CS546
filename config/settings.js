a8c05f85b881dd046ae0edded94da0c27b807f03
ARJ2211 <aayushrj22@gmail.com>
2025-10-12 18:19:45 -0400

    Changes to remove sensitive info

--- config/settings.js
+++ config/settings.js
@@ -1,6 +1,5 @@
 //YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!! DO NOT FORGET TO CHANGE THE DATABASE NAME!!!!!
 export const mongoConfig = {
-    serverUrl:
-        "mongodb+srv://aayushrj22_db_user:cR9TDX4ngPK4UhR0@slackoverflow.ypbb6db.mongodb.net/?retryWrites=true&w=majority&appName=SlackOverflow",
+    serverUrl: "enter your mongo cluster url here!",
     database: "SlackOverflow",
 };
