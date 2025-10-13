8faa7f046e432ffe06c06a4c5d0333603e87f7b0
ARJ2211 <aayushrj22@gmail.com>
2025-10-13 15:32:47 -0400

    huggingface cli used

--- tasks/fetch-models.sh
+++ tasks/fetch-models.sh
@@ -22,7 +22,7 @@ fi
 mkdir -p "$TARGET_DIR"
 
 
-hf download "$MODEL_ID" \
+huggingface-cli download "$MODEL_ID" \
   --local-dir "$TARGET_DIR" \
   --include "onnx/*" "tokenizer.json" "tokenizer_config.json" \
            "config.json" "special_tokens_map.json" "vocab.txt" "merges.txt"
