5c3ef9f4d339e118e827090ba37d6b98cb9821fe
ARJ2211 <aayushrj22@gmail.com>
2025-10-16 16:46:04 -0400

    Update the documents in questions collection added

--- mongoUtils/questionUtils.js
+++ mongoUtils/questionUtils.js
@@ -2,6 +2,7 @@ import { Normalize, Jaccard, Tokens } from "../ragUtils/jaccardNormalizer.js";
 import { questions } from "../config/mongoCollections.js";
 import { getEmbedding } from "../ragUtils/getEmbeddings.js";
 import * as validator from "../validator.js";
+import { fileURLToPath } from "url";
 
 const THRESOLD = 0.9;
 const JACCARD_THRESHOLD = 0.65;
@@ -13,15 +14,15 @@ const JACCARD_THRESHOLD = 0.65;
  * @returns {Object}
  */
 export const createQuestion = async (question, course) => {
-    course = validator.isValidMongoId(course);
+    course = validator.isValidString(course); //TODO: This has to be a mongo ObjectID
     question = validator.isValidString(question);
 
     const questionsColl = await questions();
 
-    const canonicalKey = Normalize(question);
-    const exact = await questionsColl.findOne({ canonicalKey });
+    const canonical_key = Normalize(question);
+    const exact = await questionsColl.findOne({ canonical_key });
     if (exact) {
-        throw `Error: This question already exists (exact/near-exact match) for question ${exact.question}!`;
+        throw `ERROR: This question already exists (exact/near-exact match) for question ${exact.question}!`;
     }
 
     let embedding = await getEmbedding(question);
@@ -35,14 +36,14 @@ export const createQuestion = async (question, course) => {
 
         // only block if it's both semantically very close AND has high token overlap
         if (best.score >= THRESOLD && jacScore >= JACCARD_THRESHOLD) {
-            throw `Error: This question already exists with score ${best.score} for question ${best.question}! JACCARD SORE: ${jacScore}`;
+            throw `ERROR: This question already exists with score ${best.score} for question ${best.question}! JACCARD SORE: ${jacScore}`;
         }
     }
 
     const doc = {
         question,
         embedding, // array of numbers for Atlas Vector Search
-        canonicalKey, // helps prevent trivial duplicates like punctuation/case changes
+        canonical_key, // helps prevent trivial duplicates like punctuation/case changes
         created_time: new Date(),
         replies: [], // will be empty on create as no replies yet
         course: course, // TODO: will need to get the course ID from the input params
@@ -107,9 +108,23 @@ export const searchQuestion = async (query, { k = 5, numCandidates } = {}) => {
  */
 export const updateQuestion = async (filter, obj) => {
     const questionsColl = await questions();
-    filter = validator.isValidObject();
-    obj = validator.isValidObject();
+    filter = validator.isValidObject(filter);
+    obj = validator.isValidObject(obj);
+    if (obj.hasOwnProperty("question")) {
+        var new_question = obj.question;
+        var canonical_key = Normalize(obj.question);
+        const existingQuestion = await searchQuestion(obj.question);
+        if (
+            existingQuestion.length > 0 &&
+            existingQuestion[0].score > THRESOLD
+        ) {
+            throw `ERROR: similar question already exists, ${existingQuestion[0].question}, score: ${existingQuestion[0].score}`;
+        }
+        var embedding = await getEmbedding(new_question);
+        obj = { ...obj, embedding, canonical_key };
+    }
 
+    canonical_key;
     const updateObj = {
         $set: {
             ...obj,
@@ -117,8 +132,9 @@ export const updateQuestion = async (filter, obj) => {
     };
 
     const updatedObj = await questionsColl.findOneAndUpdate(filter, updateObj, {
-        returnNewDocument: true,
+        returnDocument: "after",
     });
+    if (!updateObj || updatedObj === null) throw `ERROR: document not updated.`;
     return updatedObj;
 };
 
