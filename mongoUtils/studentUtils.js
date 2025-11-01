b2c14eb68280db53a87879b186a776dfc7d98f59
Vinci Li <vicniz@vincis-air.mynetworksettings.com>
2025-10-31 20:26:30 -0400

    Fixed studentUtils according to comments & added updateStudent

--- mongoUtils/studentUtils.js
+++ mongoUtils/studentUtils.js
@@ -1,5 +1,8 @@
-import { professors } from "../config/mongoCollections.js";
+import { students } from "../config/mongoCollections.js";
 import * as validator from "../validator.js";
+import { ObjectId } from "mongodb";
+
+
 //Hello
 
 /**
@@ -15,7 +18,7 @@ import * as validator from "../validator.js";
  * @returns {Object}
  */
 
-export const creatStudent = async (
+export const createStudent = async (
 full_name,
 first_name,
 middle_name,
@@ -26,59 +29,42 @@ otp,
 password,
 ) => {
 const studentColl = await students();
-    try {
-        full_name = validator.isValidString(full_name);
-    } catch (e) {
-        console.log("full_name", e);
-    }
-    try {
-        first_name = validator.isValidString(first_name);
-    } catch (e) {
-        console.log("first_name", e);
-    }
-    try {
-        middle_name = validator.isValidString(middle_name);
-    } catch (e) {
-        console.log("middle_name", e);
-    }
-    try {
-        last_name = validator.isValidString(last_name);
-    } catch (e) {
-        console.log("last_name", e);
-    }
-    try {
-        is_ta = validator.isValidBoolean(is_ta); //actually says binData instead? 
-    } catch (e) {
-        console.log("is_ta", e);
-    }
-    try {
-        questions_asked = validator.isArray(questions_asked);
-    } catch (e) {
-        console.log("questions_asked", e);
-    }
-    try {
-        otp = validator.isValidString(otp);
-    } catch (e) {
-        console.log("otp", e);
-    }
-    try {
-        password = validator.isValidString(password);
-    } catch (e) {
-        console.log("password", e);
-    }
+    full_name = validator.isValidString(full_name);
+    first_name = validator.isValidString(first_name);
+    middle_name = validator.isValidString(middle_name);
+    last_name = validator.isValidString(last_name);
+    is_ta = validator.isValidBoolean(is_ta);
+    questions_asked = validator.isArray(questions_asked);
+    otp = validator.isValidString(otp);
+    password = validator.isValidString(password);
 
     const exists = await studentColl.findOne({
         $or: [
-            { id: new RegExp(`^${id}$`, "i") },
             {full_name: new RegExp(`^${full_name}$`, "i")},
-            { email: new RegExp(`^${email}$`, "i")}
         ]
     });
-
+    
     if (exists) {
         throw `409 student already exists. ${exists.full_name}`;
     }
-}
+    const newStudent = {
+        full_name,
+        first_name,
+        middle_name,
+        last_name,
+        is_ta,
+        questions_asked,
+        otp,
+        password
+    };
+
+    const insertInfo = await studentColl.insertOne(newStudent);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+        throw '500, student could NOT be added!';
+    }
+
+    return await getStudentById(insertInfo.insertedId.toString());
+};
 
 
 /**
@@ -88,9 +74,9 @@ const studentColl = await students();
  */
 export const getStudentById = async (id) => {
     const studentColl = await students();
-    fullName = validator.isValidString(id); 
-    const studenData = await studentColl.findOne({
-        id: new RegExp(`^${id}$`, "i"),
+    id = validator.isValidString(id); 
+    const studentData = await studentColl.findOne({
+        _id: ObjectId.createFromHexString(id),
     });
     if (!studentData){
         throw '404 student not found!';
@@ -98,18 +84,15 @@ export const getStudentById = async (id) => {
     return studentData;
 };
 
-/**
- * Returns all of the students in an array
- * @returns {Array}
- */
-export const getAllStudents = async () => {
-    const studentColl = await students();
-    const allStudents = studentColl.find({}).toArray();;
-    return allStudents;
-}
 
-export const updateStudent = async (id) => {
-    if (!id){
-        throw '404 student not found!';
-    }
+export const updateStudent = async (id, updatedInfo) => {
+    const studentColl = await students();
+    id = validator.isValidString(id);
+    await getStudentById(id);
+    const updated = await studentColl.updateOne({
+        _id: ObjectId.createFromHexString(id)},{//converts id string to hex
+        $set: updatedInfo //adds new field or updates val of exisitng
+    });
+
+    return await getStudentById(id);
 }
