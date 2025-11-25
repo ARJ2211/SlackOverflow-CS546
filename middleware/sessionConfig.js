b9f21fa4e46b5a0d0f556e5b6ecbba65b2c9e679
Tolaj <swapnilhgf@gmail.com>
2025-11-25 15:50:27 -0500

    cache session

--- middleware/sessionConfig.js
+++ middleware/sessionConfig.js
@@ -1,5 +1,8 @@
 import session from 'express-session';
 import dotenv from 'dotenv';
+import MongoStore from "connect-mongo";
+import { mongoConfig } from '../config/settings.js';
+
 dotenv.config();
 
 const sessionConfig = session({
@@ -7,10 +10,14 @@ const sessionConfig = session({
     secret: process.env.SESSION_SECRET || 'team18-secret-key',
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: mongoConfig.serverUrl,
+        dbName: mongoConfig.database,
+        collectionName: "sessions",
+        ttl: 60 * 60 * 24 * 2,
+    }),
     cookie: {
-        maxAge: 1000 * 60 * 60 * 2, // 2 hours
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        maxAge: 1000 * 60 * 60 * 2,
     },
 });
 
