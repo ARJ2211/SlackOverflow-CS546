090a3551d5a15684b0a05d35a5e56fce88961a15
Tolaj <swapnilhgf@gmail.com>
2025-11-27 18:41:36 -0500

    fix: answer button, other click events

--- public/js/course.js
+++ public/js/course.js
@@ -51,13 +51,18 @@ const handleSaveQuestion = (event) => {
             if (status !== 200) {
                 showToast(body.message || "Unknown error.", "error")
                 button.disabled = false
-                button.innerText = "Submit"
+                button.innerHTML = `<div>Ask question</div>
+                        <svg class="shrink-0 size-3.5 pointer-events-none " xmlns="http://www.w3.org/2000/svg"
+                            width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                            <path
+                                d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+                        </svg>`
                 return
             }
 
             showToast("Question created successfully!", "success")
             button.disabled = false
-            button.innerText = "Submit"
+            button.innerText = "Ask question"
 
             setTimeout(() => {
                 window.location.reload()
@@ -67,7 +72,7 @@ const handleSaveQuestion = (event) => {
             console.error("handleAddQuestion error:", err)
             showToast("Server error. Please try again.", "error")
             button.disabled = false
-            button.innerText = "Send"
+            button.innerText = "Ask question"
         })
 
     return false
@@ -87,6 +92,8 @@ const handleFilterDropdown = (event) => {
 };
 
 const handleOutsideClick = (event) => {
+    const filterDropdown = document.getElementById("filterDropdown")
+
     const filterBtn = document.getElementById("openFilter")
     if (!filterDropdown.contains(event.target) && event.target !== filterBtn) {
         filterDropdown.classList.add("opacity-0", "scale-95", "pointer-events-none")
@@ -204,7 +211,6 @@ const handleQuillSetup = () => {
         document.onmouseup = () => document.onmousemove = null;
     };
 }
-
 
 const handleLabelsDropdown = (event) => {
     const labelsDropdown = document.getElementById("labelsDropdown")
