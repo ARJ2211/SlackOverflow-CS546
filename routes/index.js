3c4fb614029cec05aba7b3c39d9cabed68a897b1
ARJ2211 <aayushrj22@gmail.com>
2025-12-07 16:52:27 -0500

    Added home page + some better UI functionalities

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ import questionsRoutes from "./questions.js";
 import answersRoutes from "./answers.js";
 import renderAuthRoutes from "./renderAuth.js";
 import renderMainRoutes from "./renderMain.js";
-import { ensureAuth, redirectIfAuthenticated, noCacheAuth } from "../middleware/auth.js";
-import { static as staticDir } from 'express';
-import path from 'path';
+import {
+    ensureAuth,
+    redirectIfAuthenticated,
+    noCacheAuth,
+} from "../middleware/auth.js";
+import { static as staticDir } from "express";
+import path from "path";
 
 const constructorMethod = (app) => {
     app.use("/users", usersRoutes);
@@ -14,19 +18,24 @@ const constructorMethod = (app) => {
     app.use("/questions", questionsRoutes);
     app.use("/answers", answersRoutes);
 
+    app.use("/auth", redirectIfAuthenticated, noCacheAuth, renderAuthRoutes);
+    app.use("/main", ensureAuth, noCacheAuth, renderMainRoutes);
+
+    app.use("/public", staticDir("public"));
+
+    // Home page
     app.get("/", (req, res) => {
-        res.redirect('auth/sign-in');
+        return res.render("home", {
+            layout: "landing",
+            title: "SlackOverflow | Course Q&A Board",
+        });
     });
 
-    app.use("/auth", redirectIfAuthenticated, noCacheAuth, renderAuthRoutes);
-    app.use('/main', ensureAuth, noCacheAuth, renderMainRoutes);
-
-    app.use('/public', staticDir('public'));
     app.use("/{*splat}", (req, res) => {
-        res.status(404).render('error', {
-            layout: 'auth',
-            title: 'Page Not Found',
-            message: 'The page you requested could not be found.'
+        res.status(404).render("error", {
+            layout: "auth",
+            title: "Page Not Found",
+            message: "The page you requested could not be found.",
         });
     });
 };
